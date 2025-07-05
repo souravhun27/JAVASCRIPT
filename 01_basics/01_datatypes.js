d41649a98a4d0af125909c57c2e81afd8700aeac
Sourav Hun <146869925+souravhun27@users.noreply.github.com>
2025-07-05 03:13:10 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) // give error bcz, we are using node.js, not browser
+
+let name = "sourav"
+let age = 18
+let isLoggedIn = false
+
+//number => 2^53;
+//bigint
+//string = " " or ' '
+//boolean => true/false
+//null => standalone value
+//undefined => means value not assign;
+//symbol => for unique
+
+//object
+
+console.log(typeof "sourav");
+console.log(typeof null); // out: null here is object
+console.log(typeof undefined); // out: undefined
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let acccountEmail = "sourav123@gmmail.com"
+var accountPassword = "12345"
+accountCity ="meerut"
+accountState;
+
+//accountId = 2 //not allowed
+
+/*
+NEVER USE 'var' always use 'let'
+*/
+
+console.log(accountId);
+
+console.table([acccountEmail,accountId,accountPassword,accountCity,accountState]);
