930e6788826c752e2e63b90777daa1e5dc3d7135
Sourav Hun <146869925+souravhun27@users.noreply.github.com>
2025-07-05 04:15:27 +0000

    oprators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,5 +1,5 @@
 //CONVERSION OPERATIONS
-
+/*
 let score0 = 33
 console.log(typeof score0); //number
 console.log(typeof (score0)); //number
@@ -19,3 +19,32 @@ console.log(typeof valueInNumber2);//out: also number~
 let score3 = "sourav"
 let valueInNumber3 = Number(score3)
 console.log( valueInNumber3); // out: NaN , bcz "sourav" cannot be converted to number
+
+*/
+//********OPERATIONS*************//
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue);
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2); */
+
+let str1 = "sourav";
+let str2 = " hun";
+let str3 = str1 + str2;
+//console.log(str3);
+console.log("1" + 2);//12
+console.log(1 + "2");//12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); //32
+//above are bad practice ; use parenthesis 
+console.log((2 + 3) * 5 % 3);//1
+
+console.log(true);// true
+console.log(+true);//bad
+
